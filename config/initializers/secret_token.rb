b2829109210bd11c32bdb19085e70e5226e1861d
Nick Quaranto <nick@quaran.to>
2014-03-06 21:23:14 -0500

    Start me up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skyway::Application.config.secret_key_base = 'e24314c5c693d15697f4ced0fa69874e34ec62f077d641023bcc190d54262198a16a1a0e317abc0b6eea3c39bb26643c18c17f0dd9349c2d672fa4889f5c7f95'
