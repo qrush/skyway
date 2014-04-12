34066cd584b44d5cb95f67d39531a3a79aace45f
Nick Quaranto <nick@quaran.to>
2014-04-12 12:00:00 -0400

    Yank raw setlist into fixtures directory

--- test/models/parser_test.rb
+++ test/models/parser_test.rb
@@ -2,34 +2,7 @@ require 'test_helper'
 
 class ParserTest < ActiveSupport::TestCase
   setup do
-    @show = Parser.parse <<EOF
-SET I
-Kitty Chaser (Explosions)
-What's the Connection?
-Village Dog and Pony Show >
-Warren in the Window >
-Bloody Well Right *
-Eon Don ** >
-They're Calling For Ya >
-Triangle
-SET II
-King For A Day
-Staring into the Sun
-All In
-Complex Part I >
-Origami
-Complex Part II >
-Obla Di Obla Da ***
-The Median
-They're Calling For Ya >
-Triangle
-ENCORE
-Mosquito Valley Part I >
-Strange Times
-* Supertramp cover
-** "Purple Haze" and "Third Stone from the Sun" (Jimi Hendrix) teases
-*** The Beatles cover
-EOF
+    @show = Parser.parse raw_setlists("kitty")
   end
 
   test "has 3 sets" do
@@ -67,4 +40,10 @@ EOF
   test "show can be saved" do
     @show.save!
   end
+
+  private
+
+     def raw_setlists(name)
+       Rails.root.join("test/fixtures/raw_setlists/#{name}.txt").read
+     end
 end
