b9c1c1fcc0764f30b772b2165d628e999be13a2c
Nick Quaranto <nick@quaran.to>
2014-04-19 16:14:36 -0400

    Try just uniq

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
 
   has_many :slots
   has_many :setlists, through: :slots
-  has_many :shows, -> { uniq.performed }, through: :setlists
+  has_many :shows, -> { uniq }, through: :setlists
 
   def debut_show
     shows.last
