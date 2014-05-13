2e610dcf1c1c142259d481dfea42fb6e3d7e552a
Nick Quaranto <nick@quaran.to>
2014-05-12 22:50:25 -0400

    Try setting s3_domain_url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Skyway::Application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :url => ":s3_domain_url"
   }
 end
