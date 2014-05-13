4b69943b9eebaa6cd2909dfecc8b6e38c890e774
Nick Quaranto <nick@quaran.to>
2014-05-13 00:43:52 -0400

    Try alias this time...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,8 @@ Skyway::Application.configure do
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     },
-    url: ':s3_domain_url',
+    url: ':s3_alias_url',
+    s3_host_alias: ENV['S3_BUCKET_NAME'],
     path: '/:class/:attachment/:id_partition/:style/:filename'
   }
 end
