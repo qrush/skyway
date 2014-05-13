1aa4cf3c41dbd44d177ae911aa94a09bf57a5700
Nick Quaranto <nick@quaran.to>
2014-05-12 22:58:03 -0400

    what is syntax even

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,13 @@ Skyway::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :url => ':s3_alias_url',
-    :s3_host_alias => ENV['S3_BUCKET_NAME']
+    url: ':s3_alias_url',
+    s3_host_alias: ENV['S3_BUCKET_NAME']
   }
 end
