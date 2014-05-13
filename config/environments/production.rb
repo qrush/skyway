7c6dd123265b139f8bc69487f807fb63cc4d7498
Nick Quaranto <nick@quaran.to>
2014-05-12 23:07:49 -0400

    Give up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,6 @@ Skyway::Application.configure do
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    },
-    url: ':s3_alias_url',
-    s3_host_alias: ENV['S3_BUCKET_NAME'],
-    s3_host_name: ENV['S3_HOST_NAME']
+    }
   }
 end
