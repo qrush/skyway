db0a003b73e45feb8ff27df73c3fadefe55d01fe
Nick Quaranto <nick@quaran.to>
2014-08-25 22:15:20 -0400

    Fix links to static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def nav_menu
     links = [
-      link_to("Home", root_path),
-      link_to("Tour", tour_path),
+      link_to("Home",     root_path),
+      link_to("Tour",     tour_path),
       link_to("Setlists", shows_path),
-      link_to("Music", "#"),
-      link_to("Merch", "#"),
-      link_to("About", "#"),
-      link_to("Contact", "#")
+      link_to("Music",    page_path("music")),
+      link_to("Merch",    page_path("merch")),
+      link_to("About",    page_path("about")),
+      link_to("Contact",  page_path("contact"))
     ]
     links.map { |link| content_tag :li, link, class: "nav-link" }.join.html_safe
   end
