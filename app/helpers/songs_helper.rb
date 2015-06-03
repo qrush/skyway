4647c794ae5508306593342f8376faab94031954
Nick Quaranto <nick@quaran.to>
2015-06-03 00:22:36 -0400

    Clarify song subheaders

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -6,16 +6,4 @@ module SongsHelper
       link_to name, songs_path(order: order), options
     end.join.html_safe
   end
-
-  def last_performed_for(song_name)
-    if song = Song.find_by_name(song_name)
-      show = song.shows.first
-      capture do
-        concat "last performed on "
-        concat link_to "#{show.short_when} @ #{show.venue.name}", show
-      end
-    else
-      ""
-    end
-  end
 end
