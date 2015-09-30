d0870a31c1c65b4387e5b4b3fdb7cd4e83d5a8aa
Nick Quaranto <nick@quaran.to>
2015-09-29 23:03:15 -0400

    Add sonic farm to /music and new fall tour banner

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,5 +8,6 @@ class HomeController < ApplicationController
   def show
     @upcoming_shows = Show.upcoming.limit(5)
     @announcement = Announcement.last || Announcement.new
+    @week_count = Show.where(["performed_at >= ? and performed_at <= ?", Date.today, 1.week.from_now]).count
   end
 end
