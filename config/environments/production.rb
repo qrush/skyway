e6f9fabdd73f62097dd6940dc75a1958c11582f9
Nick Quaranto <nick@quaran.to>
2016-10-11 20:08:22 -0400

    Try rails CORS settings instead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,5 +104,15 @@ Rails.application.configure do
 
   config.middleware.use Rack::Deflater
 
-  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=31536000' }
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Allow-Methods' => 'GET',
+    'Access-Control-Allow-Headers' => 'x-requested-with',
+    'Access-Control-Max-Age' => '3628800',
+    'Cache-Control' => 'public, max-age=31536000',
+    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
 end
