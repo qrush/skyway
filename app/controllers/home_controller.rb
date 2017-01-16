0781e243839290fe94936ee151f31eae57adf194
Nick Quaranto <nick@quaran.to>
2017-01-15 23:54:12 -0500

    Try out some Google music artist JSON and bandsintown

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,6 @@ class HomeController < ApplicationController
   end
 
   def show
-    @upcoming_shows = Show.upcoming.limit(5)
     @announcement = Announcement.last || Announcement.new
     @week_count = Show.where(["performed_at >= ? and performed_at <= ?", Date.today, Date.today.end_of_week]).count
   end
