c1e60a36423cd73a4351c9ecb8d9aaa9ff26d08f
Nick Quaranto <nick@quaran.to>
2017-01-23 22:36:13 -0500

    More styles for lyrics

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,3 @@
 class Album < ApplicationRecord
-  has_many :songs, -> { order(album_position: :asc).with_shows }
+  has_many :songs, -> { order(album_position: :asc).where("lyrics <> ''").with_shows }
 end
