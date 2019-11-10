0f3c3a8a05cb1e09139176e316a076f97e7150c0
Nick Quaranto <nick@quaran.to>
2019-11-10 16:42:23 -0500

    Get things compiling

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,10 @@
 //= link_tree ../fonts
 //= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//
+//= link application.css
+//= link application.js
+//= link sampler.js
+//= link sampler.css
+//= link best.css
+//= link video.js
+//= link video.css
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,19 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '2.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(
-  sampler.js
-  sampler.css
-  best.css
-  video.js
-  video.css
-)
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
