7a01e2eab1ee79b231f369f1a9e0e72c74d1763b
Nick Quaranto <nick@quaran.to>
2021-09-27 08:59:56 -0400

    Hide merch link for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
       link_to("Tour",     tour_path),
       link_to("Music",    page_path("music")),
       link_to("Lyrics",   lyrics_path),
-      link_to("Merch",    "http://merch.aqueousband.com"),
+      # link_to("Merch",    "http://merch.aqueousband.com"),
       link_to("Setlists", setlists_path),
       link_to("Mobilize", "http://mobilize.aqueousband.com"),
       link_to("About",    page_path("about")),
